334bef577f3ca09e4befdd376cf8ebbed46d7034
lostboiii <dafasetiadi55@gmail.com>
2024-11-04 19:06:07 +0700

    tambah switch case login berdasarkan user role session

--- login.php
+++ login.php
@@ -16,11 +16,12 @@ if ($stmt === false) {
 
 
 $user = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+//jika terdapat user pada DB
 if ($user) {
     $_SESSION['user'] =[
-        'nim' => $user['nim'],
-        'nama' => $user['nama'],
-        'role' => $user['role'], 
+        'nim' => $user['NIM'],
+        'username' => $user['USERNAME'],
+        'role' => $user['ROLE'], 
     ];
 
     echo json_encode([
@@ -31,5 +32,21 @@ if ($user) {
 } else {
     echo json_encode(["status" => "error", "message" => "Invalid nim or password."]);
 }
-
+//cek apakah terdapat session(sebelum login)
+if(isset($_SESSION['user']['role'])){
+switch($_SESSION['user']['role']) {
+    case 'admin_jurusan':
+        echo "anda admin jurusan";
+    break;
+    case 'admin_prodi':
+        echo "anda admin prodi";
+    break;
+    case 'mahasiswa':
+        echo "anda mahasiswa";
+    break;
+}
+}
+else{
+    echo "anda gagal login";
+}
 ?>
